8569cbcfac0ad0596047bbe043d1e54b76747f8f
prashant.verma <prashant.verma@mrhomecare.in>
2016-06-02 19:03:24 +0530

    Restructured the Left Side Menu

--- common/leftMenu.php
+++ common/leftMenu.php
@@ -13,24 +13,55 @@
 			</form>
 			<!-- END RESPONSIVE QUICK SEARCH FORM -->
 		</li>
+    <li class="active">
+      <a href="javascript:;">
+        <i class="icon-table"></i>
+        <span class="title"><?php echo ucfirst("admin"); ?></span>
+        <span class="selected"></span>
+        <span class="arrow open"></span>
+      </a>
+      <ul class="sub-menu">
 		<?php foreach ($modules as $module): ?>
-		<li class="active">
-			<a href="javascript:;">
-				<i class="icon-table"></i>
-				<span class="title"><?php echo ucfirst($module['module']); ?></span>
-				<span class="selected"></span>
-				<span class="arrow open"></span>
-			</a>
-			<ul class="sub-menu">
+
+      <?php if ($module["module"]!="leadmanager" && $module["module"]!="order"):?>
+
+
+
 				<li class="<?php if($flag == ''){ echo 'active'; } ?>" id="li_0" onclick="change_tab(0);">
 					<a href="<?php print SITEPATH.'/'.$module["module"].'/display.php';?>"><img src="../img/list_bullets.png" style="padding-right:15px;" height="16" width="16"/><?php echo ucfirst($module['module']); ?></a>
 				</li>
-				<li class="<?php if($flag == 'new'){ echo 'active'; } ?>" id="li_new" >
+				<!-- <li class="<?php if($flag == 'new'){ echo 'active'; } ?>" id="li_new" >
 					<a href="<?php print SITEPATH.'/'.$module["module"].'/display.php?flag=new';?>"><img src="../img/add.png" style="padding-right:10px;"/> Add New <?php echo ucfirst($module['module']); ?> </a>
-				</li>
-			</ul>
-		</li>
-
+				</li> -->
+          <?php endif; ?>
 		<?php endforeach; ?>
+    </ul>
+    	</li>
+      <li class="active">
+        <a href="javascript:;">
+          <i class="icon-table"></i>
+          <span class="title"><?php echo ucfirst("leadmanager"); ?></span>
+          <span class="selected"></span>
+          <span class="arrow open"></span>
+        </a>
+        <ul class="sub-menu">
+          <li class="<?php if($flag == ''){ echo 'active'; } ?>" id="li_0" onclick="change_tab(0);">
+  					<a href="<?php print SITEPATH.'/'."leadManager".'/display.php';?>"><img src="../img/list_bullets.png" style="padding-right:15px;" height="16" width="16"/><?php echo ucfirst("leadmanager"); ?></a>
+  				</li>
+        </ul>
+          </li>
+          <li class="active">
+            <a href="javascript:;">
+              <i class="icon-table"></i>
+              <span class="title"><?php echo ucfirst("orders"); ?></span>
+              <span class="selected"></span>
+              <span class="arrow open"></span>
+            </a>
+            <ul class="sub-menu">
+              <li class="<?php if($flag == ''){ echo 'active'; } ?>" id="li_0" onclick="change_tab(0);">
+                <a href="<?php print SITEPATH.'/'."order".'/display.php';?>"><img src="../img/list_bullets.png" style="padding-right:15px;" height="16" width="16"/><?php echo ucfirst("orders"); ?></a>
+              </li>
+            </ul>
+              </li>
 	</ul>
 </div>
